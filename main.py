337d9d957a01919120679e38970d967857b18ce2
Andrieli Campagnaro <andricampagnaro@gmail.com>
2024-02-10 20:30:25 -0300

    clean the code and add text

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 from croniter import croniter_range
-from datetime import datetime, timedelta, date
-from random import random, randint
+from datetime import datetime, timedelta
 import pandas as pd
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import streamlit as st
-
-st.set_page_config(layout="wide")
-
-def random_datetime_between(first_datetime, last_datetime):
-    random_datetime = first_datetime + (last_datetime - first_datetime) * random()
-    return random_datetime
-
-def sample_dataframe(records=30):
-    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    tomorrow = today + timedelta(days=1)
-
-    data = []
-
-    for i in range(records):
-        task = f'Task {i + 1}'
-        start = random_datetime_between(today, tomorrow)
-        finish = random_datetime_between(start, tomorrow)
-        resource = f'Server {randint(1, 5)}'
-
-        line = {'Task': task, 'Start': start, 'Finish': finish, 'Resource': resource}
-        data.append(line)
-
-    df = pd.DataFrame(data)
-    df = df.sort_values('Start')
-    return df
-
 
 def cron_dataframe(crontab_info, resource='Sample'):
     today_start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
@@ -43,7 +16,11 @@ def cron_dataframe(crontab_info, resource='Sample'):
     for crontab_expression in crontabs:
         if not crontab_expression:
             continue
-        splitted_expression = crontab_expression.split(' ')
+
+        if crontab_expression.startswith('#'):
+            continue
+        
+        splitted_expression = crontab_expression.split()
         crontab = ' '.join(splitted_expression[:5])
         task = ' '.join(splitted_expression[5:])
         for start in croniter_range(today_start, today_finish, crontab):
@@ -58,8 +35,8 @@ def cron_dataframe(crontab_info, resource='Sample'):
 
 
 def generate_gaant(df):
-    padding = 150 # arbitrary value depending on legends
-    row_size = 30 # arbitrary value
+    padding = 150
+    row_size = 30
     num_fields = df['Task'].nunique()
     height = padding + row_size * num_fields
 
@@ -73,6 +50,9 @@ def generate_gaant(df):
                 y0=0,
                 x1=datetime.now(),
                 y1=1,
+                label=dict(
+                    text='Now'
+                ),
                 line=dict(
                     color="RoyalBlue",
                     width=3
@@ -80,7 +60,9 @@ def generate_gaant(df):
     ))
     return gaant_figure
 
-crontab_info_default = """0 11 * * * Task 1
+crontab_info_default = """# This is an example of crontab -l return
+# The lines with comments will be ignored
+0 11 * * * Task 1
 30 10 * * * Task 2
 */5 * * * * Task 3
 0 12 * * * Task 4
@@ -97,10 +79,25 @@ crontab_info_default = """0 11 * * * Task 1
 15 22 * * * Task 15
 0 0 * * * Task 16"""
 
-st.title('Streamlit Gaant')
+st.set_page_config(
+    page_title="Cron to gaant",
+    page_icon=":stopwatch:",
+    layout="wide",
+)
+
+'''
+# Cron to Gaant
+
+This is an easy way to convert linux crontab schedulers to a visual gaant graph.
+
+To do it, simple type `crontab -l` in your linux terminal and past the result below.
+
+Simple like that!
+
+'''
+
 crontab_info = st.text_area("Enter crontab expression", value=crontab_info_default)
 
-# df = sample_dataframe()
 df = cron_dataframe(crontab_info, 'Server 1')
 gaant_figure = generate_gaant(df)
 
